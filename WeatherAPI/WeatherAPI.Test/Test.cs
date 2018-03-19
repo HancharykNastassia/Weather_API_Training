9f6c14e9306a1d8eb03a2e1a5f015188abb9c11d
Nastassia Hancharyk <Nastassia_Hancharyk@epam.com>
2018-03-19 23:22:05 +0300

    Modifyed test

--- WeatherAPI/WeatherAPI.Test/Test.cs
+++ WeatherAPI/WeatherAPI.Test/Test.cs
@@ -13,16 +13,19 @@ namespace WeatherAPI.Test
   [TestFixture]
   public class Test
   {
+    private static readonly string BASE_URL = "";
     private static readonly string RESOURCE = "";
     private static readonly string APP_ID = "";
     private static readonly string CITY_ID = "";
 
+    private RestClient client;
     private RestRequest request;
     private JsonDeserializator deserializator;
 
     [SetUp]
     public void IntializeTest()
     {
+      client = new RestClient();
       request = new RestRequest();
       deserializator = JsonDeserializator.Deserializator;
     }
@@ -30,9 +33,13 @@ namespace WeatherAPI.Test
     [Test]
     public void TestAPIRequestWithCityId()
     {
+      client.BaseUrl = new Uri(BASE_URL);
       //request.
       request.Resource = RESOURCE;
       request.Method = Method.GET;
+      IRestResponse<ResponseContent> response = client.Execute<ResponseContent>(request);
+      ResponseContent content = deserializator.Deserialize<ResponseContent>(response);
+      Assert.AreEqual(CITY_ID, content.CityInfo.Id, "Wrong response");
     }
   }
 }
